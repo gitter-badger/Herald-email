0a705717676dca5c3b4d97d4f18aece37d07c74c
Kestanous <CMTopus@me.com>
2014-09-30 11:18:18 -0400

    used new runner code

--- package.js
+++ package.js
@@ -1,13 +1,12 @@
 Package.describe({
   summary: "Add Email medium to Notifications",
   version: "0.1.0",
-  git: "https://github.com/Meteor-Reaction/notifications-email.git"
+  git: "https://github.com/Meteor-Reaction/notifications-email.git",
   name: "kestanous:notifications-email"
 });
 
 Package.onUse(function(api) {
   api.use('kestanous:notifications'); 
 
-  api.addFiles('lib/notifications-email.js');
-  api.addFiles('lib/server.js', 'server');
+  api.addFiles('lib/notifications-email.js', 'server');
 });
