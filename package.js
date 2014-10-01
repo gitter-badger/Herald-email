df7021591aed01bca3df32fdad21ac255251f56f
Kestanous <CMTopus@me.com>
2014-10-01 10:58:00 -0400

    actually bump

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Add Email medium to Notifications",
   version: "0.1.0",
-  git: "https://github.com/Meteor-Reaction/notifications-email.git",
-  name: "kestanous:notifications-email"
+  git: "https://github.com/Meteor-Reaction/harold-email.git",
+  name: "kestanous:harold-email"
 });
 
 Package.onUse(function(api) {
-  api.use('kestanous:notifications'); 
+  api.use('kestanous:harold'); 
 
   api.addFiles('lib/notifications-email.js', 'server');
 });
