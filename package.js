d2e98e115700f6d5be9cd6328e78ca5b4bc50a66
Kestanous <CMTopus@me.com>
2014-10-01 11:37:53 -0400

    fixed really bad typo

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
   summary: "Add Email medium to Notifications",
   version: "0.1.0",
-  git: "https://github.com/Meteor-Reaction/harold-email.git",
-  name: "kestanous:harold-email"
+  git: "https://github.com/Meteor-Reaction/Herald-email.git",
+  name: "kestanous:herald-email"
 });
 
 Package.onUse(function(api) {
-  api.use('kestanous:harold'); 
-
+  api.use('kestanous:herald@0.4.0'); 
   api.addFiles('lib/notifications-email.js', 'server');
 });
