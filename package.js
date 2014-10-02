d294c57db3b4e20526b3f5fdee7a1d6c9707836a
Kestanous <CMTopus@me.com>
2014-10-02 16:23:22 -0400

    bump for core

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Add Email medium to Herald",
-  version: "0.2.0",
+  version: "0.2.1",
   git: "https://github.com/Meteor-Reaction/Herald-email.git",
   name: "kestanous:herald-email"
 });
 
 Package.onUse(function(api) {
-  api.use('kestanous:herald@0.5.0'); 
+  api.use('kestanous:herald@0.6.0'); 
   api.addFiles('lib/notifications-email.js', 'server');
 });
